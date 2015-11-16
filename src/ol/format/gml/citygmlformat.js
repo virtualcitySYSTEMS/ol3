d0bdebf4cec2b2595332a38b38336b197ca5e087
Lucas van Walstijn <lvanwalstijn@intern.virtualcitySYSTEMS.de>
2015-11-16 12:40:05 +0100

    CityGML and WFS2 support for getFeature response of 3dCityDB

---
+++ src/ol/format/gml/citygmlformat.js
@@ -0,0 +1,254 @@
+goog.provide('ol.format.CityGML');
+
+goog.require('goog.asserts');
+goog.require('goog.dom.NodeType');
+//goog.require('ol.format.Feature');
+goog.require('ol.format.XMLFeature');
+goog.require('ol.xml');
+
+
+/**
+ * @classdesc
+ * CityGML class for parsing, currently only used in WFS2Format for reading wfs:GetFeature response
+ *
+ * @constructor
+ * @param {olx.format.GMLOptions=} opt_options
+ *     Optional configuration object.
+ * @extends {ol.format.XMLFeature}
+ */
+ol.format.CityGML = function(opt_options) {
+  var options = /** @type {olx.format.GMLOptions} */
+      (opt_options ? opt_options : {});
+
+  /**
+   * @protected
+   * @type {string}
+   */
+  this.schemaLocation_ = options.schemaLocation ? options.schemaLocation : ol.format.CityGML.SCHEMA_LOCATION;
+
+  /*
+  "http://www.opengis.net/citygml/waterbody/2.0" : null,
+  "http://www.opengis.net/citygml/vegetation/2.0" : null,
+  "http://www.opengis.net/citygml/relief/2.0" : null,
+  "http://www.opengis.net/citygml/transportation/2.0" : null,
+  "http://www.opengis.net/citygml/building/2.0" : null,
+  "http://www.opengis.net/citygml/cityobjectgroup/2.0" : null,
+  "http://www.opengis.net/citygml/tunnel/2.0" : null,
+  "http://www.opengis.net/citygml/cityfurniture/2.0" : null,
+  "http://www.opengis.net/citygml/bridge/2.0" : null,
+  "http://www.opengis.net/citygml/landuse/2.0" : null
+  */
+  /**
+   * @type {Object.<string, Object.<string, Object>>}
+   */
+  this.TOP_LEVEL_FEATURE_PARSERS = {
+    "http://www.opengis.net/citygml/building/2.0" : {
+      "Building" : ol.xml.makeReplacer(this.readBuildingFeature)
+    }
+  };
+
+  this.BUILDING_PARSER_AS_FEATURE = {
+    "http://www.opengis.net/citygml/2.0" : {
+      "creationDate" : this.makeFeaturePropertySetter(this.readCreationDate, "creationDate")
+    },
+    "http://www.opengis.net/citygml/generics/2.0" : {
+      "stringAttribute" : this.makeFeatureGenericCityGMLPropertySetter(this.readGenericStringAttribute),
+      "intAttribute" : this.makeFeatureGenericCityGMLPropertySetter(this.readGenericIntAttribute)
+    }
+  };
+
+  this.BUILDING_PARSER_AS_OBJECT = {
+    "http://www.opengis.net/citygml/2.0" : {
+      "creationDate" : this.makeObjectPropertySetterAsAttribute(this.readCreationDate, "creationDate")
+    },
+    "http://www.opengis.net/citygml/generics/2.0" : {
+      "stringAttribute" : this.makeObjectGenericCityGMLPropertySetterAsAttribute(this.readGenericStringAttribute),
+      "intAttribute" : this.makeObjectGenericCityGMLPropertySetterAsAttribute(this.readGenericIntAttribute)
+    }
+  };
+
+
+  goog.base(this);
+};
+goog.inherits(ol.format.CityGML, ol.format.XMLFeature);
+
+
+/**
+ * @const
+ * @type {Array.<string>}
+ */
+ol.format.CityGML.NS = [
+    "http://www.opengis.net/citygml/waterbody/2.0",
+    "http://www.3dcitydb.org/citygml-ade/3.0",
+    "http://www.opengis.net/citygml/2.0",
+    "http://www.opengis.net/citygml/vegetation/2.0",
+    "http://www.opengis.net/citygml/relief/2.0",
+    "http://www.opengis.net/citygml/transportation/2.0",
+    "http://www.opengis.net/citygml/building/2.0",
+    "http://www.opengis.net/citygml/cityobjectgroup/2.0",
+    "http://www.opengis.net/citygml/tunnel/2.0",
+    "http://www.opengis.net/citygml/cityfurniture/2.0",
+    "http://www.opengis.net/citygml/generics/2.0",
+    "http://www.opengis.net/citygml/bridge/2.0",
+    "http://www.w3.org/1999/xlink",
+    "http://www.opengis.net/citygml/landuse/2.0",
+    "http://www.opengis.net/citygml/waterbody/2.0"
+  ];
+
+  ol.format.CityGML.SCHEMA_LOCATION = "http://schemas.opengis.net/citygml/waterbody/2.0/waterBody.xsd http://www.3dcitydb.org/citygml-ade/3.0 http://www.virtualcitysystems.de/3dcitydb/citygml-ade/3.0/3dcitydb-ade-citygml-2.0.xsd http://www.opengis.net/citygml/vegetation/2.0 http://schemas.opengis.net/citygml/vegetation/2.0/vegetation.xsd http://www.opengis.net/citygml/transportation/2.0 http://schemas.opengis.net/citygml/transportation/2.0/transportation.xsd http://www.opengis.net/citygml/relief/2.0 http://schemas.opengis.net/citygml/relief/2.0/relief.xsd http://www.opengis.net/citygml/building/2.0 http://schemas.opengis.net/citygml/building/2.0/building.xsd http://www.opengis.net/citygml/cityobjectgroup/2.0 http://schemas.opengis.net/citygml/cityobjectgroup/2.0/cityObjectGroup.xsd http://www.opengis.net/wfs/2.0 http://schemas.opengis.net/wfs/2.0/wfs.xsd http://www.opengis.net/citygml/tunnel/2.0 http://schemas.opengis.net/citygml/tunnel/2.0/tunnel.xsd http://www.opengis.net/citygml/cityfurniture/2.0 http://schemas.opengis.net/citygml/cityfurniture/2.0/cityFurniture.xsd http://www.opengis.net/citygml/generics/2.0 http://schemas.opengis.net/citygml/generics/2.0/generics.xsd http://www.opengis.net/citygml/bridge/2.0 http://schemas.opengis.net/citygml/bridge/2.0/bridge.xsd http://www.opengis.net/citygml/landuse/2.0 http://schemas.opengis.net/citygml/landuse/2.0/landUse.xsd";
+
+
+ol.format.CityGML.prototype.readBuildingFeature = function(node, objectStack){
+  goog.asserts.assert(node.nodeType == goog.dom.NodeType.ELEMENT,'node.nodeType should be ELEMENT');
+  var object = {
+    id : node.getAttribute("gml:id"),
+    type : 26
+  };
+  object = ol.xml.pushParseAndPop(object, this.BUILDING_PARSER_AS_OBJECT, node, objectStack, this);
+  return object;
+  /*
+  var feature = new ol.Feature();
+  feature.setId(node.getAttribute("gml:id"));
+  feature = ol.xml.pushParseAndPop(feature, this.BUILDING_PARSER, node, objectStack, this);
+  return feature;
+  */
+};
+
+ol.format.CityGML.prototype.readCreationDate = function(node, objectstack){
+  return node.textContent.trim();
+};
+
+ol.format.CityGML.prototype.readGenericStringAttribute = function(node, objectStack){
+  goog.asserts.assert(node.nodeType == goog.dom.NodeType.ELEMENT,'node.nodeType should be ELEMENT');
+  var attributeName = node.getAttribute("name");
+  var attributeValue = node.textContent.trim();
+  var object = {};
+  object[attributeName] = attributeValue;
+  return object;
+};
+
+ol.format.CityGML.prototype.readGenericIntAttribute = function(node, objectStack){
+  goog.asserts.assert(node.nodeType == goog.dom.NodeType.ELEMENT,'node.nodeType should be ELEMENT');
+  var attributeName = node.getAttribute("name");
+  var attributeValue = parseFloat(node.textContent.trim());
+  var object = {};
+  object[attributeName] = attributeValue;
+  return object;
+};
+
+/**
+ * Make an object property setter function and store the value under "attributes".
+ * @param {function(this: T, Node, Array.<*>): *} valueReader Value reader.
+ * @param {string=} opt_property Property.
+ * @param {T=} opt_this The object to use as `this` in `valueReader`.
+ * @return {ol.xml.Parser} Parser.
+ * @template T
+ */
+ol.format.CityGML.prototype.makeObjectPropertySetterAsAttribute =
+  function(valueReader, opt_property, opt_this) {
+  goog.asserts.assert(valueReader !== undefined,'undefined valueReader, expected function(this: T, Node, Array.<*>)');
+  return (
+    /**
+     * @param {Node} node Node.
+     * @param {Array.<*>} objectStack Object stack.
+     */
+    function(node, objectStack) {
+      var value = valueReader.call(opt_this !== undefined ? opt_this : this,node, objectStack);
+      if (value !== undefined) {
+        var object = /** @type {Object} */
+            (objectStack[objectStack.length - 1]);
+        var property = opt_property !== undefined ? opt_property : node.localName;
+        goog.asserts.assert(goog.isObject(object),'entity from stack was not an object');
+        if (!object.attributes){
+          object.attributes = {};
+        }
+        object.attributes[property] = value;
+      }
+    });
+};
+
+/**
+ * Make an (ol.feature) feature property setter function for generic CityGML attributes name attribute of node contains the name of the attribute
+ * @param {function(this: T, Node, Array.<*>): Object<string,*>} valueReader Value reader.
+ * @param {string=} opt_property Property.
+ * @param {T=} opt_this The object to use as `this` in `valueReader`.
+ * @return {ol.xml.Parser} Parser.
+ * @template T
+ */
+ol.format.CityGML.prototype.makeObjectGenericCityGMLPropertySetterAsAttribute =
+    function(valueReader, opt_property, opt_this) {
+  goog.asserts.assert(valueReader !== undefined,'undefined valueReader, expected function(this: T, Node, Array.<*>)');
+  return (
+      /**
+       * @param {Node} node Node.
+       * @param {Array.<*>} objectStack Object stack.
+       */
+      function(node, objectStack) {
+        /** @type {Object<string,*>} */
+        var propertyObject = valueReader.call(opt_this !== undefined ? opt_this : this,node, objectStack);
+        if (propertyObject !== undefined) {
+          var object = /** @type {Object} */
+              (objectStack[objectStack.length - 1]);
+          for (var key in propertyObject){
+            object.attributes[key] = propertyObject[key];
+          }
+        }
+      });
+};
+
+/**
+ * Make an (ol.feature) feature property setter function for generic CityGML attributes name attribute of node contains the name of the attribute
+ * @param {function(this: T, Node, Array.<*>): Object<string,*>} valueReader Value reader.
+ * @param {string=} opt_property Property.
+ * @param {T=} opt_this The object to use as `this` in `valueReader`.
+ * @return {ol.xml.Parser} Parser.
+ * @template T
+ */
+ol.format.CityGML.prototype.makeFeatureGenericCityGMLPropertySetter =
+    function(valueReader, opt_property, opt_this) {
+  goog.asserts.assert(valueReader !== undefined,'undefined valueReader, expected function(this: T, Node, Array.<*>)');
+  return (
+      /**
+       * @param {Node} node Node.
+       * @param {Array.<*>} objectStack Object stack.
+       */
+      function(node, objectStack) {
+        var propertyObject = valueReader.call(opt_this !== undefined ? opt_this : this,node, objectStack);
+        if (propertyObject !== undefined) {
+          var feature = /** @type {ol.Feature} */
+              (objectStack[objectStack.length - 1]);
+          goog.asserts.assert(feature instanceof ol.Feature,'entity from stack was not an object');
+          feature.setProperties(propertyObject);
+        }
+      });
+};
+
+/**
+ * Make an (ol.feature) feature property setter function
+ * @param {function(this: T, Node, Array.<*>): *} valueReader Value reader.
+ * @param {string=} opt_property Property.
+ * @param {T=} opt_this The object to use as `this` in `valueReader`.
+ * @return {ol.xml.Parser} Parser.
+ * @template T
+ */
+ol.format.CityGML.prototype.makeFeaturePropertySetter =
+    function(valueReader, opt_property, opt_this) {
+  goog.asserts.assert(valueReader !== undefined,'undefined valueReader, expected function(this: T, Node, Array.<*>)');
+  return (
+      /**
+       * @param {Node} node Node.
+       * @param {Array.<*>} objectStack Object stack.
+       */
+      function(node, objectStack) {
+        var value = valueReader.call(opt_this !== undefined ? opt_this : this,node, objectStack);
+        if (value !== undefined) {
+          var feature = /** @type {ol.Feature} */
+              (objectStack[objectStack.length - 1]);
+          goog.asserts.assert(feature instanceof ol.Feature,'entity from stack was not an object');
+          var property = opt_property !== undefined ? opt_property : node.localName;
+          var propertyObject = {};
+          propertyObject[property] = value;
+          feature.setProperties(propertyObject);
+        }
+      });
+};
---
+++ src/ol/format/wfs2format.js
@@ -0,0 +1,94 @@
+goog.provide('ol.format.WFS2');
+
+goog.require('goog.asserts');
+goog.require('goog.dom.NodeType');
+goog.require('ol');
+goog.require('ol.format.XMLFeature');
+goog.require('ol.xml');
+goog.require('ol.format.CityGML');
+
+
+
+/**
+ * @classdesc
+ * Feature format for reading data in the WFS2 format.
+ * Currently only reads in CityGML2.0 and only parses a getFeature response
+ *
+ * @constructor
+ * @param {Object} opt_options
+ *     Optional configuration object.
+ * @extends {ol.format.XMLFeature}
+ * @api stable
+ */
+ol.format.WFS2 = function(opt_options) {
+  var options = opt_options ? opt_options : {};
+
+  /**
+   * @private
+   * @type {ol.format.CityGML}
+   */
+  this.cityGMLFormat_ = new ol.format.CityGML();
+
+  /**
+   * @private
+   * @type {string}
+   */
+  this.schemaLocation_ = options.schemaLocation ? options.schemaLocation : ol.format.WFS2.SCHEMA_LOCATION;
+
+  /**
+   * @type {Object.<string, Object.<string, Object>>}
+   */
+  this.FEATURE_COLLECTION_PARSERS = {};
+
+  this.FEATURE_COLLECTION_PARSERS[ol.format.WFS2.WFS2NS] = {
+    'member': ol.xml.makeArrayPusher(this.handleWFSMember)
+  };
+
+  goog.base(this);
+};
+goog.inherits(ol.format.WFS2, ol.format.XMLFeature);
+
+
+/**
+ * @const
+ * @type {string}
+ */
+ol.format.WFS2.XMLNS = 'http://www.w3.org/2000/xmlns/';
+
+/**
+ * @const
+ * @type {string}
+ */
+ol.format.WFS2.WFS2NS = 'http://www.opengis.net/wfs/2.0';
+
+/**
+ * @const
+ * @type {string}
+ */
+ol.format.WFS2.SCHEMA_LOCATION = ol.format.WFS2.WFS2NS + ' http://schemas.opengis.net/wfs/2.0/wfs.xsd';
+
+/**
+ * @inheritDoc
+ */
+ol.format.WFS2.prototype.readFeaturesFromNode = function(node, opt_options) {
+
+  var objectStack = [{}];
+
+  var features = ol.xml.pushParseAndPop([], this.FEATURE_COLLECTION_PARSERS, node, objectStack, this);
+  if (!features) {
+    features = [];
+  }
+  return features;
+};
+
+/**
+ * @param {Node} node Node.
+ * @param {Array.<*>} objectStack Object stack.
+ * @return {Array.<ol.Feature>} Features.
+ */
+ol.format.WFS2.prototype.handleWFSMember = function(node, objectStack) {
+  goog.asserts.assert(node.nodeType == goog.dom.NodeType.ELEMENT,'node.nodeType should be ELEMENT');
+  var localName = ol.xml.getLocalName(node);
+  var feature = ol.xml.pushParseAndPop([], this.cityGMLFormat_.TOP_LEVEL_FEATURE_PARSERS, node, objectStack, this.cityGMLFormat_);
+  return feature;
+};
